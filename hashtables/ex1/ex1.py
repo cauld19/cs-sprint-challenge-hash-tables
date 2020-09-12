a623b30ef05b8dfd383bf0adf2ff6a8425b49095
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-09-12 10:40:08 -0700

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+def get_indices_of_item_weights(weights, length, limit):
+    
+    ## loop over wieghts and place in a dictionary
+    
+    ## index each weight (enumerate)
+    
+    ## run over which pair of weights equals limit
+    
+    d = {}
+
+    for index, weight in enumerate(weights): ##make hash table of values in array 
+        d[weight] = index
+
+    for i, weight in enumerate(weights): ## loop through array
+        if (limit-weight) in d: ## if limit - weight = 0 and is in weights return indexes
+            return [d[limit-weight], i]
+    return None ## if not found return none
+
+
+
