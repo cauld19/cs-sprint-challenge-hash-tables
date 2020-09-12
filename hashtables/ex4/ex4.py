21a5b4cc9f9ec783f86229aaf6bbf9a2be22b463
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-09-12 13:50:50 -0700

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,17 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    arr = []
+    
+    arr_sorted = sorted(a,reverse=True)
 
-    return result
+    for val in arr_sorted:
+        if val > 0:
+            cache[val] = 0
+        elif val < 0 and abs(val) in cache:
+            arr.append(abs(val))
 
+    print(cache)
+    return arr
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
